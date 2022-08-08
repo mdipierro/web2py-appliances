ee23a13a8df2a3b3caea8a51d35418f7cee0e17b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-08 12:57:56 +0800

    Delete CustomerRelationshipManagement directory

--- CustomerRelationshipManagement/models/utils.py
+++
@@ -1,9 +0,0 @@
-def url(f,args=[]): return URL(r=request,f=f,args=args)
-def button(text,action,args=[]):
-    return SPAN('[',A(text,_href=URL(r=request,f=action,args=args)),']')
-
-def link_person(person):
-    return A(person.name,_href=url('view_person',person.id))
-
-def link_company(company):
-    return A(company.name,_href=url('view_company',company.id))
